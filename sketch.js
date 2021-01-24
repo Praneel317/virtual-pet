6b54fc07cb36c7a2fcc63695214c4749e48732ca
Praneel317 <74415279+Praneel317@users.noreply.github.com>
2021-01-24 12:08:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+//Create variables here
+var Dog,happyDog
+var foodStock
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(500,500);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
